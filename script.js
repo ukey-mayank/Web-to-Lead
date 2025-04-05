82084127e64cfc5c42590208d28e6da621f59f2a
Mayank Ukey <mayankukey15@gmail.com>
2025-04-05 17:58:06 +0530

    Added google reCaptcha script to script.js

--- script.js
+++ script.js
@@ -8,3 +8,6 @@ let beforeSubmit = () => {
     let formattedDate = new Date(inputdate.value).toLocaleDateString('en-IN')
     outputdate.value = formattedDate
 }
+
+// function to get goolge reCaptcha
+function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") { var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value); elems["ts"] = JSON.stringify(new Date().getTime()); document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
